14c31c66fc84aeaa33357adb77ebb4763da0b504
Paul <>
2023-03-24 20:49:53 +0300

    added focus after clearing of input + debounce search || debounce, useCallback, useRef, value, onChange

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { debounce } from "lodash";
+import React, { useCallback, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setSearched } from "../redux/pizzasDisplaySlice";
 
@@ -7,17 +8,38 @@ function Search() {
   let dispatch = useDispatch();
   let searched = useSelector((state) => state.pizzasSlice.searched);
 
+  //INPUT & DEBOUNCE LOGIC
+  let [searchValue, setSearchValue] = useState("");
+  let setSearchedDebounce = useCallback(
+    debounce((value) => dispatch(setSearched(value)), 1000),
+    []
+  );
+
+  let onChangeLogic = (event) => {
+    setSearchValue(event.target.value);
+    setSearchedDebounce(event.target.value);
+  };
+
+  let onClickCross = () => {
+    setSearchValue("");
+    dispatch(setSearched(""));
+    input_ref.current.focus();
+  };
+
+  let input_ref = useRef();
+
   return (
     <>
       <input
-        value={searched}
-        onChange={(event) => dispatch(setSearched(event.target.value))}
+        ref={input_ref}
+        value={searchValue}
+        onChange={onChangeLogic}
       ></input>
       <img
         src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Cross_red_circle.svg"
         width="25"
         height="25"
-        onClick={() => dispatch(setSearched(""))}
+        onClick={onClickCross}
       ></img>
     </>
   );
